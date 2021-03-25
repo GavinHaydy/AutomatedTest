a6a6ea00a223e84446ba1df388b1dfe5af246ed6
bugpz <13558622779@163.com>
2021-03-25 16:39:40 +0800

    fix：增加ddt模式例子

--- TestCase/test.py
+++ TestCase/test.py
@@ -3,8 +3,12 @@ from time import sleep
 from selenium import webdriver
 from Page.Home.baidu import BaiduPage
 from Page.Home.elementui import Element
+from Page.Home.bugpz import BugPZ
+from DataProcessing import bugLOGIN
+from ddt import ddt, data
 
 
+@ddt
 class TestBaidu(unittest.TestCase):
 
     def setUp(self):
@@ -23,6 +27,22 @@ class TestBaidu(unittest.TestCase):
         page.open()
         page.divider()
         sleep(2)
+
+    filepath = 'Config/bugpz-login.xlsx'
+    dataS = bugLOGIN.ExcelUtil(filepath, 'Sheet1')
+    datadict = dataS.dict_data()
+
+    def login(self, username, password):
+        page = BugPZ(self.driver)
+        page.open()
+        sleep(10)
+        page.username(username)
+        page.password(password)
+        page.login_btn()
+
+    @data(*datadict)
+    def test_login_case(self, test):
+        self.login(test['username'], test['password'])
 
     def tearDown(self):
         self.driver.quit()
