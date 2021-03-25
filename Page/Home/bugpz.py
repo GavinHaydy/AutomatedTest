7ae25bfdddb24694bdadc01a83e7436b2bb994f9
bugpz <13558622779@163.com>
2021-03-25 16:40:34 +0800

    fix：例子文件，获取元素

---
+++ Page/Home/bugpz.py
@@ -0,0 +1,14 @@
+from Business.BaseUtil import BasePage
+
+
+class BugPZ(BasePage):
+    url = 'https://bug-pz.cn/login'
+
+    def username(self, search_key):
+        self.by_xpath('//*[@id="app"]/div/div/div/div[2]/form/div[1]/div/div/div/input').send_keys(search_key)
+
+    def password(self, search_key):
+        self.by_xpath('//*[@id="app"]/div/div/div/div[2]/form/div[2]/div/div/div/input').send_keys(search_key)
+
+    def login_btn(self):
+        self.by_xpath('//*[@id="app"]/div/div/div/div[2]/form/div[3]/button').click()
