b4fe613beae28315a9d5fd26e756724ca2dc40b9
bugpz <13558622779@163.com>
2022-08-08 00:19:29 +0800

    perf：

--- Page/Home/baidu.py
+++ Page/Home/baidu.py
@@ -2,12 +2,11 @@ from Common.BaseUtil import BasePage
 
 
 class BaiduPage(BasePage):
+    url = 'www.baidu.com'
 
-    def open_url(self,url):
-        return self.driver.get(url)
     # 定位搜索框输入搜索数据
     def search_input(self, text=None):
-        self.send_keys_(['id', "kw"], text)
+        self.send_keys(['id', "kw"], text)
 
     # 点击百度一下按钮搜索
     def search_button(self):
--- TestCase/test.py
+++ TestCase/test.py
@@ -2,53 +2,25 @@ import unittest
 from time import sleep
 from selenium import webdriver
 from Page.Home.baidu import BaiduPage
-from Page.Home.elementui import Element
-from Page.Home.bugpz import BugPZ
-from DataProcessing import bugLOGIN
-from ddt import ddt, data
-from unittest import skip
+from Common.BaseUtil import open_browser
 
 
-@ddt
 class TestBaidu(unittest.TestCase):
 
     def setUp(self):
         # self.driver = webdriver.Chrome()
-        self.driver = webdriver.PhantomJS()     # 无界面浏览器  官网 phantomjs.org
-
-    def test_baidu_search_case(self):
-        page = BaiduPage(self.driver)
-        page.open()
-        page.search_input("selenium ")
-        page.search_button()
-        sleep(2)
-        self.assertEqual(page.get_title(), "selenium_百度搜索")
-
-    @skip
-    def test_elem_case(self):
-        page = Element(self.driver)
-        page.open()
-        page.divider()
-        sleep(2)
-
-    filepath = 'Config/bugpz-login.xlsx'
-    dataS = bugLOGIN.ExcelUtil(filepath, 'Sheet1')
-    datadict = dataS.dict_data()
-
-    def login(self, username, password):
-        page = BugPZ(self.driver)
-        page.open()
-        page.username(username)
-        page.password(password)
-        page.login_btn()
-
-    @skip
-    @data(*datadict)
-    def test_login_case(self, test):
-        self.login(test['username'], test['password'])
+        self.driver = open_browser('chrome')     # 无界面浏览器  官网 phantomjs.org
+        self.driver.get(BaiduPage.url)
+        self.page = BaiduPage(self.driver)
 
     def tearDown(self):
         self.driver.quit()
+
+    def test_baidu_search_case(self):
+        self.page.search_input("selenium ")
+        self.page.search_button()
+        sleep(2)
+        self.assertEqual(self.page.get_title(), "selenium_百度搜索")
 
 
 if __name__ == '__main__':
