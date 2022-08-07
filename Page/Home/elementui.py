649ca0c27a6432511a6c462c65b3f6f077f5acfb
bugpz <13558622779@163.com>
2022-08-07 23:13:56 +0800

    fix：Delete useless files

--- Page/Home/elementui.py
+++
@@ -1,8 +0,0 @@
-from Common.BaseUtil import BasePage
-
-
-class Element(BasePage):
-    url = 'https://element.eleme.cn/#/zh-CN/component/installation'
-
-    def divider(self):
-        self.by_xpath('//*[@id="app"]/div[2]/div/div[1]/div/div/div[1]/div[1]/div/div/ul/li[5]/div[6]/ul/li[9]/a')
